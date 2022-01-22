cfde481882c795fd873f6919ebffb8efab430871
André Matthies <matthiez@gmail.com>
2022-01-22 17:03:52 +0100

    add german language

---
+++ src/Resources/lang/de/app.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'about' => 'SMS versenden via sms77 - 
+    ein in Deutschland ansässiger Telekommunikationsanbieter seit 2003.',
+    'flash' => 'Flash',
+    'from' => 'Absenderkennung',
+    'name' => 'sms77',
+    'no_recipients' => 'Keine Empfangsziele mit den gegebenen Filtern gefunden.',
+    'phone' => 'Rufnummer',
+    'send_sms' => 'SMS senden',
+    'sms_sent' => ':msgCount SMS versandt an :receivers Empfangsziele für :cost €',
+    'text' => 'Text',
+];
